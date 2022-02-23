eb10dbe79a22ad35421648c3b62b01d082458ea8
mrchenliang <hi@chenliang.ca>
2022-02-22 18:25:49 -0700

    update node site example with event listener on the frontend

--- node-site-example/public/js/scripts.js
+++ node-site-example/public/js/scripts.js
@@ -1,9 +1,23 @@
 window.onload = function () {
   console.log('Loaded site')
-  const button = document.getElementById('delete');
-  const pathname = window.location.pathname;
-  button.onclick = async function() {
-    await axios.delete(`http://localhost:8000${pathname}`);
-    return window.open('http://localhost:8000');
+  const plusButton = document.getElementsByClassName('plus');
+  const minusButton = document.getElementsByClassName('minus');
+  console.log(plusButton, minusButton)
+  const number = document.getElementsByClassName('number');
+  const itemPrice = document.getElementsByClassName('itemPrice');
+  const total = document.getElementsByClassName('total');
+  const plusButtonClickHandler = function() {
+    const result = parseInt(number[0].innerHTML) + 1
+    const totalResult = parseFloat(itemPrice[0].innerHTML) * result
+    number[0].innerHTML = result; 
+    total[0].innerHTML = totalResult;
   }
+  const minusButtonClickHandler = function() {
+    const result = parseInt(number[0].innerHTML) - 1
+    const totalResult = parseFloat(itemPrice[0].innerHTML) * result
+    number[0].innerHTML = result; 
+    total[0].innerHTML = totalResult;
+  }
+  plusButton[0].addEventListener('click', plusButtonClickHandler);
+  minusButton[0].addEventListener('click', minusButtonClickHandler);
 }
