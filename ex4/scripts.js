2e38bc8e8c4ad502a1bf7778508045feddfe90c2
mrchenliang <hi@chenliang.ca>
2022-02-28 19:49:10 -0700

    debugging exercises

---
+++ ex4/scripts.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+  let page = {title: 'Hello world'}
+  let heading = document.querySelector('h1')
+  heading.innerText = page.title;
+}
---
+++ ex5/scripts.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+  let page = {title: 'Hello world'}
+  let heading = document.getElementById('heading')
+  heading.innerText = page.title;
+}
